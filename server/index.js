f54efabb4328d6ce3fa4a5428ef8f49f6feef1af
Mohamed Yusuf <mh.yusuf92@gmail.com>
2020-11-11 11:59:32 +0000

    feat: connect to MongoDB Atlas

--- server/index.js
+++ server/index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 
+const connectDB = require('./models');
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
+
+connectDB().then(() => {
+  console.log('Connected to MongoDB');
+  app.listen(PORT, () => {
+    console.log(`Server listening on http://localhost:${PORT}`);
+  });
 });
---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+const { mongoURI } = require('../config/keys');
+
+module.exports = () =>
+  mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
