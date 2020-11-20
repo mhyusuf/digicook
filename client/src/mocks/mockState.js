6e259626c85acd7c30243b6bde9f2b81ffc76216
bmcglauser <bmcglauser@gmail.com>
2020-11-20 15:53:28 +0000

    tests: start to implement testing

---
+++ client/src/mocks/mockState.js
@@ -0,0 +1,52 @@
+
+const fakeState = {
+  auth: {
+    _id: '5fb64e41e8d419ff602cd0c3',
+    googleId: '112538235083508704382',
+    name: 'Brett-Marco Glauser',
+    email: 'bmcglauser@gmail.com',
+    __v: 0
+  },
+  collections: {
+    collectionList: [
+      {
+        isPrivate: false,
+        _recipes: [
+          '5fb64ed5e8d419ff602cd0c5'
+        ],
+        _id: '5fb64e5de8d419ff602cd0c4',
+        name: 'The best collection',
+        _user: {
+          _id: '5fb64e41e8d419ff602cd0c3',
+          googleId: '112538235083508704382',
+          name: 'Brett-Marco Glauser',
+          email: 'bmcglauser@gmail.com',
+          __v: 0
+        },
+        description: 'Check outtt yoooo',
+        __v: 0,
+        image: {
+          type: 'Buffer',
+          data: []
+        }
+      }
+    ],
+    collectionDetail: {},
+    recipeList: [],
+    recipe: {
+      name: '',
+      category: '',
+      image: '',
+      ingredients: [
+        {
+          name: '',
+          quantity: ''
+        }
+      ],
+      instructions: ''
+    }
+  },
+  menus: false
+}
+
+export default fakeState;
---
+++ client/src/mocks/mockUsers.js
@@ -0,0 +1,90 @@
+exports.fakeAuthedUser = {
+  auth: {
+    _id: '5fb64fcfe8d419ff602cd0cb',
+    googleId: '107236975444538131971',
+    name: 'Derek Clearwater',
+    email: 'derek.clearwater@gmail.com',
+    __v: 0
+  },
+  collections: {
+    collectionList: [
+      {
+        isPrivate: false,
+        _recipes: [],
+        _id: '5fb64ffce8d419ff602cd0cc',
+        name: 'Bullshit',
+        _user: {
+          _id: '5fb64fcfe8d419ff602cd0cb',
+          googleId: '107236975444538131971',
+          name: 'Derek Clearwater',
+          email: 'derek.clearwater@gmail.com',
+          __v: 0
+        },
+        description: 'Literal shit from bulls',
+        __v: 0,
+        image: {
+          type: 'Buffer',
+          data: [
+          ]
+        }
+      }
+    ],
+    collectionDetail: {},
+    recipeList: [],
+    recipe: {
+      name: '',
+      category: '',
+      image: '',
+      ingredients: [
+        {
+          name: '',
+          quantity: ''
+        }
+      ],
+      instructions: ''
+    }
+  },
+  menus: true
+};
+
+exports.fakeUnauthedUser = {
+  collections: {
+    collectionList: [
+      {
+        isPrivate: false,
+        _recipes: [],
+        _id: '5fb64ffce8d419ff602cd0cc',
+        name: 'Bullshit',
+        _user: {
+          _id: '5fb64fcfe8d419ff602cd0cb',
+          googleId: '107236975444538131971',
+          name: 'Derek Clearwater',
+          email: 'derek.clearwater@gmail.com',
+          __v: 0
+        },
+        description: 'Literal shit from bulls',
+        __v: 0,
+        image: {
+          type: 'Buffer',
+          data: [
+          ]
+        }
+      }
+    ],
+    collectionDetail: {},
+    recipeList: [],
+    recipe: {
+      name: '',
+      category: '',
+      image: '',
+      ingredients: [
+        {
+          name: '',
+          quantity: ''
+        }
+      ],
+      instructions: ''
+    }
+  },
+  menus: true
+};
