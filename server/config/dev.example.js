7f5f95bb990c451c426278aba8592ac4151dacc4
Mohamed Yusuf <mh.yusuf92@gmail.com>
2020-12-11 14:54:18 +0000

    feat(server): add docker-compose file for local db and example config file

---
+++ server/config/dev.example.js
@@ -0,0 +1,6 @@
+module.exports = {
+  cookieKey: '54rujv094vu8mtrw0e',
+  googleClientID: 'ewrewqrewqr15.apps.googleusercontent.com',
+  googleClientSecret: 'e_dsfasd53r-J6JKP-eP90e',
+  mongoURI: 'mongodb://root:rootpassword@localhost:27017'
+};
