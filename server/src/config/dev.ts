a6e5b3792540830093c7e4fadd73123c9e37f0cd
Mohamed Yusuf <mh.yusuf92@gmail.com>
2020-12-12 12:52:30 +0000

    docs: delete dev.ts

--- server/src/config/dev.ts
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  cookieKey: 'uiX605fHByT7Il2Omn8H',
-  googleClientID: '694454088324-rpvns63dahaeg3nvlkudsa8f2ku2lsp2.apps.googleusercontent.com',
-  googleClientSecret: 'e_2s6NrC8ir-JE2PUI-eQvUG',
-  mongoURI: 'mongodb://root:rootpassword@localhost:27017'
-};
