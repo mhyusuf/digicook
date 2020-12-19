bf90f538ef71f91cff11b52b76cb53ebe1ea2583
Mohamed Yusuf <mh.yusuf92@gmail.com>
2020-12-19 19:36:39 +0000

    build: update production config file

--- server/src/config/prod.ts
+++ server/src/config/prod.ts
@@ -1,1 +1,6 @@
-module.exports = {};
+module.exports = {
+  cookieKey: process.env.COOKIE_KEY,
+  googleClientID: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+};
